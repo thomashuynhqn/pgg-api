746bc4ac178690183fc057c8745ffedb5331131c
Thomas Huỳnh <thomashuynhqn@gmail.com>
2025-11-26 10:46:36 +0700

    Adding "Carrer" post for RaydianCloud

---
+++ src/api/career-raydiancloud/routes/career-raydiancloud.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * career-raydiancloud router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::career-raydiancloud.career-raydiancloud');
---
+++ src/api/career-raydiancloud/services/career-raydiancloud.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * career-raydiancloud service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::career-raydiancloud.career-raydiancloud');
